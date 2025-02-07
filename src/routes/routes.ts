57c7785d45fb80c7b0ade1bbcf5778a1a9b16c72
philipthedeveloper <88097351+philipthedeveloper@users.noreply.github.com>
2025-02-07 14:02:28 +0100

    Remove unused pages

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,7 +2,6 @@ import Dashboard from "../pages/Dashboard";
 import Login from "../pages/Login";
 import Redirect from "../pages/Redirect";
 import Logout from "../pages/Logout";
-import Register from "../pages/Register";
 
 export interface RouteElement {
   path: string;
@@ -15,7 +14,6 @@ const publicRoutes: RouteElement[] = [
     path: "/login",
     Element: Login,
   },
-  { path: "/register", Element: Register },
   {
     path: "/logout",
     Element: Logout,
